8cea14d25f051b3f3c1e9dafde5d210600356ea4
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-06-02 13:20:39 +0100

    dir structure

---
+++ week1/interview-questions/social-network-connectivity/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+
+bool addConnection(int mem1, int mem2, std::map<int, bool>& isConnectedToRoot) {
+
+}
+
+int main() {
+    int totalNumberOfMembers(0);
+    int currentNumberOfMembers(0);
+    std::map<int, bool> isConnectedToRoot;
+
+    if (isConnectedToRoot[member]) {
+
+    }
+    return 0;
+}
